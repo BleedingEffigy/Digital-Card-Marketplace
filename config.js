e70a29ad71ac93becfdd382b12de4c17b7dd7445
phantomhaze <ahern077@gmail.com>
2021-09-27 09:19:03 -0700

    updated mumbai rpc

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
+export const nftmarketaddress = "0x34ACDC3C6Cc7A415B4c2a965ed58541076f6C083"
+export const nftaddress = "0xf12aD0Bd6f0506e09d74567189E970d68C7433d8"
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,6 @@
 /* hardhat.config.js */
 require("@nomiclabs/hardhat-waffle")
+require('dotenv').config();
 const fs = require('fs')
 const privateKey = fs.readFileSync(".secret").toString().trim() || "01234567890123456789"
 
@@ -10,7 +11,7 @@ module.exports = {
       chainId: 1337
     },
     mumbai: {
-      url: "https://rpc-mumbai.matic.today",
+      url: "https://matic-mumbai.chainstacklabs.com/",
       accounts: [privateKey]
     }
   },
